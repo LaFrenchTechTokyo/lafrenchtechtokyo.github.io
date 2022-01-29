31524f2f6885f06b7436f41891d460413565a099
Ugo Bataillard <ugopublic@bataillard.me>
2022-01-29 17:17:25 +0900

    Fix some of the meta

--- components/meta.js
+++ components/meta.js
@@ -4,7 +4,7 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 export default function Meta() {
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+      <link rel="apple-touch-icon" sizes="192x192" href="/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
       <link rel="manifest" href="/favicons/site.webmanifest" />
@@ -14,9 +14,10 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <title>La French Tech Tokyo › A network of entrepreneurs in Japan</title>
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`A community of entrepreneurs in Japan and French entrepreneurs around the world, looking to create business opportunities in the Japanese startup ecosystem.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
