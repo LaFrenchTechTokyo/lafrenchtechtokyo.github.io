f9e30a8edd9628a6e0da7893692304f324b61682
nebievina <nebievinagb@gmail.com>
2022-03-07 19:07:35 +0900

    add Team section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import Layout from '../components/Layout'
 import Hero from '../components/Home/Hero'
 import Whatwedo from '../components/Home/Whatwedo'
+import Whoweare from '../components/Home/Whoweare'
+import About from '../components/Home/About'
+import Team from '../components/Home/Team'
 
 export default function Index() {
   return (
     <Layout>
       <Hero />
       <Whatwedo />
+      <Whoweare />
+      <About />
+      <Team />
     </Layout>
   )
 }
